174d56aee6a8b2d96294aa6afb570b6e4881981a
Dax <contact@antoniodavide.dev>
2021-01-06 10:18:35 +0100

    X86: Filter BSS segments when searching for prologues

--- x86/x86_prologue.cpp
+++ x86/x86_prologue.cpp
@@ -9,7 +9,7 @@ X86Prologue::X86Prologue(RDContext* ctx): m_context(ctx)
 void X86Prologue::search()
 {
     RDDocument_EachSegment(m_document, [](const RDSegment* s, void* userdata) {
-        if(!HAS_FLAG(s, SegmentFlags_Code)) return true;
+        if(!HAS_FLAG(s, SegmentFlags_Code) || HAS_FLAG(s, SegmentFlags_Bss)) return true;
         auto* thethis = reinterpret_cast<X86Prologue*>(userdata);
         const RDBlockContainer* blocks = RDDocument_GetBlocks(thethis->m_document, s->address);
         if(blocks) thethis->searchPrologue(blocks);
@@ -41,7 +41,6 @@ void X86Prologue::searchPrologue(const RDBlockContainer* blocks)
 
         RDBufferView view;
         if(!RDContext_GetBlockView(thethis->m_context, b, &view)) return true;
-        rd_status("Searching prologues @ " + rd_tohex(b->address));
 
         while(u8* p = RDBufferView_FindPatternNext(&view, thethis->m_pattern.c_str())) {
             auto loc = RD_AddressOf(thethis->m_loader, p);
