514db39904715a1d40d7f9009a6b8d8d90896890
Dax <contact@antoniodavide.dev>
2021-02-05 16:07:55 +0100

    CapstoneLifter: Add virtual destructor

--- capstonebundle/plugin/capstonelifter.h
+++ capstonebundle/plugin/capstonelifter.h
@@ -8,6 +8,7 @@ class CapstoneLifter
 {
     public:
         CapstoneLifter(RDContext* ctx);
+        virtual ~CapstoneLifter() = default;
         virtual void lift(const Capstone* capstone, rd_address address, const RDBufferView* view, RDILFunction* il) = 0;
 
     protected:
