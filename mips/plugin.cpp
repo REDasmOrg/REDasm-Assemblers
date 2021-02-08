10a63268c5ef4f541f839d43ac4bb7bc833134c1
Dax <contact@antoniodavide.dev>
2021-02-08 22:18:36 +0100

    MIPS: Added mips64 entries

--- mips/plugin.cpp
+++ mips/plugin.cpp
@@ -15,4 +15,16 @@ void rdplugin_init(RDContext*, RDPluginModule* pm)
     mips32be.renderinstruction = &MIPS::renderInstruction<&rd_frombe32>;
     mips32be.bits = 32;
     RDAssembler_Register(pm, &mips32be);
+
+    RD_PLUGIN_ENTRY(RDEntryAssembler, mips64le, "MIPS64 (Little Endian)");
+    mips64le.emulate = &MIPS::emulate<&rd_fromle32>;
+    mips64le.renderinstruction = &MIPS::renderInstruction<&rd_fromle32>;
+    mips64le.bits = 64;
+    RDAssembler_Register(pm, &mips64le);
+
+    RD_PLUGIN_ENTRY(RDEntryAssembler, mips64be, "MIPS64 (Big Endian)");
+    mips64be.emulate = &MIPS::emulate<&rd_frombe32>;
+    mips64be.renderinstruction = &MIPS::renderInstruction<&rd_frombe32>;
+    mips64be.bits = 64;
+    RDAssembler_Register(pm, &mips64be);
 }
