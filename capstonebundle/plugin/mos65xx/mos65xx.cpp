c5c3ebadd78fee39faaaed6d37e6e12496613d1d
Paul McQuade <paulmcquad@gmail.com>
2022-11-04 17:36:06 +0000

    MOS65xx Plugin 6.1 - Emulate Part Branch
    Added MOS65XX_INS_BVS case
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- capstonebundle/plugin/mos65xx/mos65xx.cpp
+++ capstonebundle/plugin/mos65xx/mos65xx.cpp
@@ -5,37 +5,38 @@ MOS65XX::MOS65XX(RDContext* ctx, cs_mode mode): Capstone(ctx, CS_ARCH_MOS65XX, m
 
 void MOS65XX::emulate(RDEmulateResult* result)
 {
-    rd_address address = RDEmulateResult_GetAddress(result);
-    const auto& mos65xx = m_insn->detail->mos65xx;
 
-
-    auto* insn = this->decode(address, RDEmulateResult_GetView(result));
-    if(!insn) return;
+    //auto* insn = this->decode(address, RDEmulateResult_GetView(result));
+    //if(!insn) return;
 
     // Instruction is decoded, you can use Capstone API to analyze it
     
-    // RDContext_SetAddressAssembler(m_context, address, this->endianness() == Endianness_Big ? MOS65XXBE_ID : MOS65XXLE_ID);
+    rd_address address = RDEmulateResult_GetAddress(result);
     if(!this->decode(address, RDEmulateResult_GetView(result))) return;
-
     RDEmulateResult_SetSize(result, m_insn->size);  // Next time "emulate" is called is after insn->size bytes
 
-     switch(insn->id)
+    const auto& mos65xx = m_insn->detail->mos65xx;
+
+     switch(m_insn->id)
     {
       case MOS65XX_INS_BVS: {
-        
-        RDEmulateResult_AddBranch(result, mos65xx.operands[0].imm);
-
+                    RDEmulateResult_AddBranchTrue(result, mos65xx.operands[0].imm);
+                    RDEmulateResult_AddBranchFalse(result, address + m_insn->size);
         return;
       }
       
       default: break;
     }
+    return;
 }
 
 void MOS65XX::render(const RDRendererParams* rp)
 {
   // You can render instructions here
-  auto* insn = this->decode(rp->address, &rp->view);
+  // auto* insn = this->decode(rp->address, &rp->view);
+
+  const auto& mos65xx = m_insn->detail->mos65xx;
+
 
 }
 
